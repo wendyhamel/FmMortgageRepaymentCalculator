298c5e7b246e8c62b22c928f72f1e11eb9adc10c
wendyhamel <whamel@it4web.nl>
2025-02-13 19:37:34 +0100

    Refined calculations

--- src/calculations.js
+++ src/calculations.js
@@ -6,28 +6,33 @@ window.repayMortgageCalc = function() {
 		amount: Alpine.$persist(null),
 		term: Alpine.$persist(null),
 		rate: Alpine.$persist(null),
-		mortgageTypes: ['Repayment', 'InterestOnly'],
+		mortgageTypes: [
+			{id: 'repayment', name: 'Repayment'},
+			{id: 'interest_only', name: 'Interest Only'}
+		],
 		selectedType: Alpine.$persist(null),
 		monthlyRepayment: null,
 		total: null,
 
 		pickCalculationForMortgageType(amountNumber, interest) {
-			let term = Number(this.term)
-			let termInMonths = term * 12
-			if (this.selectedType === 'Repayment') {
-				this.monthlyRepayment = Number(((interest + amountNumber) / term) / 12).toFixed(2)
-			}
-			else if ( this.selectedType === 'InterestOnly') {
-				this.monthlyRepayment = Number((interest / term) / 12).toFixed(2)
-			}
+
 		},
 
 		calculateInterest() {
-			let amountNumber = Number(this.amount.replace(/,/g, ""))
-			let interest = Number((amountNumber / 100) * this.rate)
-			this.pickCalculationForMortgageType(amountNumber, interest)
-			this.total = Number(Number(amountNumber) + Number(interest)).toFixed(2)
-			this.showWarning = this.selectedType === 'InterestOnly' ? true : false
+			let amountAsNumber = Number(this.amount.replace(/,/g, ""))
+			let termAsNumber = Number(this.term)
+			let rateAsNumber = Number(this.rate)
+			let ratePerMonth = Number(rateAsNumber / 100 / 12)
+			let interestPerMonth = Number(ratePerMonth * amountAsNumber)
+			let termInMonths = termAsNumber * 12
+			if (this.selectedType === 'repayment') {
+				this.monthlyRepayment = Number(interestPerMonth / (1 - Math.pow((1 + ratePerMonth),(-termAsNumber * 12)))).toFixed(2)
+			}
+			else if ( this.selectedType === 'interest_only') {
+				this.monthlyRepayment = Number(interestPerMonth).toFixed(2)
+			}
+			this.total = Number( Number(this.monthlyRepayment) * termInMonths).toFixed(2)
+			this.showWarning = this.selectedType === 'interest_only' ? true : false
 			this.showResults = true
 		},
 

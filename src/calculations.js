f653b9e66525fc246aab360eb0ab15c35e555d34
wendyhamel <whamel@it4web.nl>
2025-02-14 16:14:16 +0100

    Finished with validations and currency formatting

--- src/calculations.js
+++ src/calculations.js
@@ -14,8 +14,65 @@ window.repayMortgageCalc = function() {
 		monthlyRepayment: null,
 		total: null,
 
-		pickCalculationForMortgageType(amountNumber, interest) {
+		liveValidation: false,
 
+		validation: {
+			amount: {
+				rule: {
+					required: function(field) {
+						if (field) {
+							return { invalid: false, message: ''}
+						} else {
+							return { invalid: true, message: 'This field is required'}
+						}
+					}
+				}
+			},
+			term: {
+				rule: {
+					required: function(field) {
+						if (field) {
+							return { invalid: false, message: ''}
+						} else {
+							return { invalid: true, message: 'This field is required'}
+						}
+					}
+				}
+			},
+			rate: {
+				rule: {
+					required: function(field) {
+						if (field) {
+							return { invalid: false, message: ''}
+						} else {
+							return { invalid: true, message: 'This field is required'}
+						}
+					}
+				}
+			},
+			selectedType: {
+				rule: {
+					required: function(field) {
+						if (field) {
+							return { invalid: false, message: ''}
+						} else {
+							return { invalid: true, message: 'This field is required'}
+						}
+					}
+				}
+			}
+		},
+		validate(field) {
+			for (const key in this.validation[field].rule) {
+				const validationResult = this.validation[field].rule[key](this[field])
+				if (validationResult.invalid) {
+					this.validation[field].invalid = true
+					this.validation[field].message = validationResult.message
+					break
+				}
+				this.validation[field].invalid = false
+				this.validation[field].message = ''
+			}
 		},
 
 		calculateInterest() {
@@ -25,15 +82,38 @@ window.repayMortgageCalc = function() {
 			let ratePerMonth = Number(rateAsNumber / 100 / 12)
 			let interestPerMonth = Number(ratePerMonth * amountAsNumber)
 			let termInMonths = termAsNumber * 12
+			let monthlyRepayment = Number(null)
 			if (this.selectedType === 'repayment') {
-				this.monthlyRepayment = Number(interestPerMonth / (1 - Math.pow((1 + ratePerMonth),(-termAsNumber * 12)))).toFixed(2)
+				monthlyRepayment = Number(interestPerMonth / (1 - Math.pow((1 + ratePerMonth),(-termAsNumber * 12)))).toFixed(2)
 			}
 			else if ( this.selectedType === 'interest_only') {
-				this.monthlyRepayment = Number(interestPerMonth).toFixed(2)
+				monthlyRepayment = Number(interestPerMonth).toFixed(2)
 			}
-			this.total = Number( Number(this.monthlyRepayment) * termInMonths).toFixed(2)
-			this.showWarning = this.selectedType === 'interest_only' ? true : false
+			let total = Number( monthlyRepayment * termInMonths).toFixed(2)
+			this.monthlyRepayment = new Intl.NumberFormat("gb-GB", {style: "currency", currency:"GBP"}).format(monthlyRepayment)
+			this.total = new Intl.NumberFormat("gb-GB", {style: "currency", currency:"GBP"}).format(Number(total))
+			this.showWarning = this.selectedType === 'interest_only'
 			this.showResults = true
+		},
+
+		validateLive() {
+			if (this.liveValidation) {
+				this.validate('amount')
+				this.validate('term')
+				this.validate('rate')
+				this.validate('selectedType')
+			}
+		},
+
+		submit() {
+			this.validate('amount')
+			this.validate('term')
+			this.validate('rate')
+			this.validate('selectedType')
+			this.liveValidation = true
+			if (!this.validation.amount.invalid && !this.validation.term.invalid && !this.validation.rate.invalid) {
+				this.calculateInterest()
+			}
 		},
 
 		clearAllFields() {
@@ -44,6 +124,11 @@ window.repayMortgageCalc = function() {
 			this.monthlyRepayment = null
 			this.total= null
 			this.showResults = false
+			this.liveValidation = false
+			this.validation['amount'].message = ''
+			this.validation['term'].message = ''
+			this.validation['rate'].message = ''
+			this.validation['selectedType'].message = ''
 		}
 	}
 }
